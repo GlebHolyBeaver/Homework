5be9872ddab03ffa84c7d20ab5506a766626561d
GlebHolyBeaver <73456026+GlebHolyBeaver@users.noreply.github.com>
2020-11-24 16:41:46 +0300

    ДЗ 8

---
+++ Homework_Eight/src/main/java/ru/kovalev/java/homewrok8/Cat.java
@@ -0,0 +1,24 @@
+package ru.kovalev.java.homewrok8;
+
+public class Cat {
+
+    private String name;
+    private int appetite;
+
+    public Cat(String name, int appetite) {
+        this.name = name;
+        this.appetite = appetite;
+    }
+
+    public void eat(Plate plate) {
+        System.out.println("Кот " + this.name + ", с аппетитом: " + this.appetite + " пытается поесть.");
+        plate.checkFood(this.appetite);
+        System.out.println();
+    }
+    // На случай если нужно вывести инфо о коте
+    public void info() {
+        System.out.println("Имя: " + this.name);
+        System.out.println("Аппетит: " + this.appetite);
+    }
+
+}
---
+++ Homework_Eight/src/main/java/ru/kovalev/java/homewrok8/MainClass.java
@@ -0,0 +1,7 @@
+package ru.kovalev.java.homewrok8;
+
+public class MainClass {
+    public static void main(String[] args) {
+        App app = new App();
+    }
+}
